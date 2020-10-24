371fd3344cec9ef132f8c97d00046ebdb201e766
zhenpeng ge <zhenpeng.ge@qq.com>
2020-10-25 00:28:41 +0800

    fixed some errors due to the change of rm_interfaces

--- rm_auto_aim/src/task_auto_aim.cpp
+++ rm_auto_aim/src/task_auto_aim.cpp
@@ -41,63 +41,37 @@ TaskAutoAim::TaskAutoAim(rclcpp::Node::SharedPtr &nh):
     //set target blue
     auto_aim_algo_.setTargetColor(false);
     projectile_tansform_tool_.setModel(NULL);
-    setRunFlag(true);
     RCLCPP_INFO(nh_->get_logger(), "init!");
 }
 
 TaskAutoAim::~TaskAutoAim() {}
 
-void TaskAutoAim::taskImageProcess(cv::Mat &img, double img_stamp) {
-    RCLCPP_INFO(nh_->get_logger(), "stamp:%f",img_stamp);
+void TaskAutoAim::taskImageProcess(cv::Mat &img, double /*img_stamp*/) {
     int ret;
-    //robot_msgs::TaskAutoAimInfo auto_aim_info;
-    //auto_aim_info.task_mode = task_mode_;
     ret = auto_aim_algo_.process(img,pitch_info_);
     if (ret == 0) {
         // ROS_INFO("find target!!!!!!");
         ArmorTarget target = auto_aim_algo_.getTarget();
         Point3f position = target.postion / 100;
-        //transformation
-        //float pitch, yaw;
-        // Point3f position_tran, poistion_in_world;
-        // position_tran=position+offset_trans_;
-        // float cosa, sina;
-        // sina = sin(pitch_info_ );
-        // cosa = cos(pitch_info_ );
-        // poistion_in_world.z = position_tran.z * cosa - position_tran.y * sina;
-        // poistion_in_world.y = position_tran.z * sina + position_tran.y * cosa;
-        // poistion_in_world.x=position_tran.x;
-        // projectile_tansform_tool_.transform(poistion_in_world, pitch, yaw);
-        // pitch=pitch-pitch_info_;
-        // pitch = pitch + offset_pitch_ * CV_PI / 180;
-        // yaw = yaw + offset_yaw_ * CV_PI / 180;
-        // double cast = sqrt(yaw * yaw + pitch * pitch)*180/CV_PI;
-        // //发布云台控制topic
-        // if (gimbal_ctrl_flag_) {
-        //     robot_msgs::GimbalInfo gimbal_info;
-        //     gimbal_info.type = 0x00;
-        //     gimbal_info.pitch = pitch*pitch_coeff_;
-        //     gimbal_info.yaw = yaw*yaw_coeff_;
-        //     gimbal_ctrl_pub_.publish(gimbal_info);
-        // }
-        // //发射子弹
-        // if (cast < 1.2 && target.postion.z < 500) {
-        //     if (launch_ctrl_flag_) {
-        //         robot_msgs::LaunchInfo launch_info;
-        //         launch_info.projectile_num = 3;
-        //         launch_pub_.publish(launch_info);
-                
-        //     }
-        // }
-        //info
-        //auto_aim_info.result_code = 0x02;
-        //auto_aim_info.x = target.postion.x;
-        //auto_aim_info.y = target.postion.y;
-        //auto_aim_info.z = target.postion.z;
-        //auto_aim_info.pitch = pitch;
-        //auto_aim_info.yaw = yaw;
- 
-        //auto_aim_info.cast = sqrt(yaw * yaw + pitch * pitch)*180/CV_PI;
+        //
+        float pitch,yaw;
+        if(projectile_tansform_tool_.transform(position,pitch,yaw)!=0){
+            return;
+        }
+        //发布云台控制topic
+        if (gimbal_ctrl_flag_) {
+            rm_interfaces::msg::GimbalControl gimbal_info;
+            gimbal_info.type = 0x00;
+            gimbal_info.position.pitch = pitch;
+            gimbal_info.position.yaw = yaw;
+            gimbal_ctrl_pub_->publish(gimbal_info);
+        }
+        //发射子弹
+        if (shoot_ctrl_flag_) {
+            rm_interfaces::msg::ShootControl shoot_info;
+            shoot_info.projectile_num = 3;
+            shoot_pub_->publish(shoot_info);
+        }
         RCLCPP_INFO(nh_->get_logger(), "taskImageProcess():find." );
         cout << position << endl;
     }else{
@@ -111,42 +85,32 @@ void TaskAutoAim::taskImageProcess(cv::Mat &img, double img_stamp) {
 bool TaskAutoAim::setModeCallBack(
     const std::shared_ptr<rm_interfaces::srv::SetMode::Request> request,
     std::shared_ptr<rm_interfaces::srv::SetMode::Response> response) {
-    // 0x00,启动，正常控制，0x01,启动,不发子弹,0x02,启动，不控制,
-    // 0x03 暂停/休眠。
-    response->success = false;
-    if (1) {
-        response->success = true;
-        if (request->mode == 0x00) {
-            setRunFlag(true);
-            gimbal_ctrl_flag_ = true;
-            shoot_ctrl_flag_ = true;
-        } else if (request->mode == 0x01) {
-            setRunFlag(true);
-            gimbal_ctrl_flag_ = true;
-            shoot_ctrl_flag_ = false;
-        } else if (request->mode == 0x02) {
-            setRunFlag(true);
-            gimbal_ctrl_flag_ = false;
-            shoot_ctrl_flag_ = false;
-        } else if (request->mode == 0x03) {
-            setRunFlag(false);
-        } else if (request->mode == 0x10) {             
-            // color config
-            //auto_aim_algo_.setTargetColor(true);   // target is red
-            //ROS_INFO("[task_auto_aim]change target color <red>");
-        }else{
-            response->success = false;
-        }
-        //update mode
-        if (response->success) {
-            task_mode_ = request->mode;
-        }
-    } else {
+    // 0x00,休眠模式，0x01:自动射击模式，0x02：自动瞄准模式（不发子弹）,0x03,记录模式,不控制.
+    response->success=true;
+    if (request->mode == 0x00) {
+        stopTask();
+    } else if (request->mode == 0x01) {
+        startTask();
+        gimbal_ctrl_flag_ = true;
+        shoot_ctrl_flag_ = true;
+    } else if (request->mode == 0x02) {
+        startTask();
+        gimbal_ctrl_flag_ = true;
+        shoot_ctrl_flag_ = false;
+    } else if (request->mode == 0x03) {
+        startTask();
+        gimbal_ctrl_flag_ = false;
+        shoot_ctrl_flag_ = false;
+    } else if (request->mode == 0x10) {             
+        // color config
+        //auto_aim_algo_.setTargetColor(true);   // target is red
+        //ROS_INFO("[task_auto_aim]change target color <red>");
+    }else{
         response->success = false;
     }
     return true;
 }
 
 void TaskAutoAim::robotStateCallback(const rm_interfaces::msg::StdBotState::SharedPtr msg){
-    pitch_info_ = msg->current_pitch;
+    pitch_info_ = msg->gimbal.pitch;
 }
