67e1d37f72ef7e9bc309401275bb01ee1c7f4a72
zhenpeng ge <zhenpeng.ge@qq.com>
2020-11-10 23:07:58 +0800

    fix some issues due to rm_cam package updated

--- rm_auto_aim/launch/task_auto_aim_test.launch.py
+++ rm_auto_aim/launch/task_auto_aim_test.launch.py
@@ -5,28 +5,28 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    ld = LaunchDescription()
     #cam_node
     image_name="test.jpg"
     image_path = os.path.join(get_package_share_directory('rm_auto_aim'), 'res/',image_name)
-    cam_node=Node(package='rm_cam',executable='task_sim_cam_image',name="sim_camera",
+    cam_node=Node(package='rm_cam',executable='virtual_image_cam',name="virtual_cam",
             parameters=[
-                {'cam_topic_name': 'sim_camera/image_raw'},
+                {'cam_topic_name': 'virtual_cam/image_raw'},
                 {'image_path': image_path},
                 {'cam_fps': 30}
             ],
             output='screen'
     )
     #task auto aim node
-    auto_aim_params = os.path.join(get_package_share_directory('rm_auto_aim'),'res/task_auto_aim_config.yaml')
+    auto_aim_params_file = os.path.join(get_package_share_directory('rm_auto_aim'),'res/task_auto_aim_config.yaml')
     auto_aim_node=Node(package='rm_auto_aim',executable='task_auto_aim',name="task_auto_aim",
             parameters=[
-                {'cam_topic_name': 'sim_camera/image_raw'},
-                auto_aim_params
+                {'cam_topic_name': 'virtual_cam/image_raw'},
+                auto_aim_params_file
             ],
             output='screen'
     )
     #add node
+    ld = LaunchDescription()
     ld.add_action(cam_node)
     ld.add_action(auto_aim_node)
     return ld
