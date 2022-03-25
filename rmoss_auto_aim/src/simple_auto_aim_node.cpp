cab3b2b03d4ade1e6c92a0c591c8a52021f8daef
gezp <zhenpeng.ge@qq.com>
2022-03-25 17:28:56 +0800

    support rclcpp_components for rmoss_auto_aim

--- rmoss_auto_aim/src/simple_auto_aim_node.cpp
+++ rmoss_auto_aim/src/simple_auto_aim_node.cpp
@@ -186,3 +186,10 @@ bool SimpleAutoAimNode::control_task_cb(rmoss_util::TaskCmd cmd)
 }
 
 }  // namespace rmoss_auto_aim
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(rmoss_auto_aim::SimpleAutoAimNode)
